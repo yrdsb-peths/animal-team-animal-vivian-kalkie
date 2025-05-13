84f0aa5cd0e62d2ac484f9bbdbe7898cc34f5984
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-05-13 14:10:08 -0400

    added scorelabel game over label and fish code from part 1 video

--- MyWorld.java
+++ MyWorld.java
@@ -7,5 +7,30 @@ public class MyWorld extends World {
         // Create the elephant object 
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 250);
+        
+        // Create a label 
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 40, 40);
+    }
+    
+    /**
+     * End the game and draw game over
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, getWidth()/2, 200);
+    }
+    
+    /** 
+     * Create a new apple at a random location at the top of the screen 
+     */
+    public void createFish()
+    {
+        Apple apple = new Apple();
+        apple.setSpeed(level);
+        int x = Greenfoot.getRandomNumber(500);
+        int y = 0 ;
+        addObject(fish, x, y);
     }
 }
