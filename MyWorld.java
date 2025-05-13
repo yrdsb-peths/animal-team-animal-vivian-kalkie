596a900e23d5da6f9874f21f70accae9fbeac82c
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-05-13 13:56:33 -0400

    error edit

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,11 @@
 import greenfoot.*;
-
+ 
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        // Create the elephant object 
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 250);
     }
 }
