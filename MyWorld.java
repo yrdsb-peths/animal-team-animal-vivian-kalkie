289bd89f62f0a3adf2d77bdd3cf4fa6bbee6a01f
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-05-16 14:06:40 -0400

    added score

--- MyWorld.java
+++ MyWorld.java
@@ -1,9 +1,9 @@
 import greenfoot.*;
  
 public class MyWorld extends World {
+    public int score = 0;
     Label scoreLabel;
     int level = 1;
-    
     public MyWorld() 
     {
         super(600, 400, 1);
@@ -17,6 +17,20 @@ public class MyWorld extends World {
         addObject(scoreLabel, 40, 40);
         
         createApple();
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+         
+        if(score % 5 == 0)
+        {
+            level += 1;
+        }
     }
     
     /**
